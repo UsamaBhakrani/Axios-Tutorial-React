77d575184f332a8dd35baf5d266f5d47856beddb
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-01 15:52:55 +0500

    Axioss Global

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Title from "./components/Title";
+import "./axios/global";
 
 import FirstRequest from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
-import PostRequest from './examples/3-post-request'
-
+import PostRequest from "./examples/3-post-request";
 
 const App = () => {
   return (
     <div>
       <Title />
-      <FirstRequest/>
+      <PostRequest />
     </div>
   );
 };
