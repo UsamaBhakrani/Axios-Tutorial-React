b39417f0486d9fcc16bf8d0d0439c4467ac36213
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-02 16:17:03 +0500

    Setting Up Global Instance as Default

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import "./axios/global";
 import FirstRequest from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
 import PostRequest from "./examples/3-post-request";
+import GlobalInstance from "./examples/4-global-instance";
 
 const App = () => {
   return (
     <div>
       <Title />
-      <PostRequest />
+      <GlobalInstance />
     </div>
   );
 };
