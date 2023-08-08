6844008da80951b7404f1e46884c67c633ef2138
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-08 18:24:05 +0500

    Custom instance

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Title from "./components/Title";
-import "./axios/global";
 
 import FirstRequest from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
 import PostRequest from "./examples/3-post-request";
 import GlobalInstance from "./examples/4-global-instance";
+import CustomInstance from "./examples/5-custom-instance";
 
 const App = () => {
   return (
     <div>
       <Title />
-      <GlobalInstance />
+      <CustomInstance />
     </div>
   );
 };
